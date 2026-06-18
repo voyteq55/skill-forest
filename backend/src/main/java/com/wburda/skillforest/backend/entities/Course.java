305e979671549d40adf248f23dfc0aca8d3da80f
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2026-06-18 09:46:03 +0200

    Add Course entity

---
+++ backend/src/main/java/com/wburda/skillforest/backend/entities/Course.java
@@ -0,0 +1,28 @@
+package com.wburda.skillforest.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne(cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "created_by_user_id", nullable = false)
+    private Teacher createdBy;
+
+    @Column(name = "shareable_url", unique = true)
+    private String shareableUrl;
+}
