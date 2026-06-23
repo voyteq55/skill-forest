1ec804cbb68f666b1e16d3742ace16de98549f91
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2026-06-23 13:50:53 +0200

    Add endpoint for enrolling in course

--- backend/src/main/java/com/wburda/skillforest/backend/controllers/EnrollmentController.java
+++ backend/src/main/java/com/wburda/skillforest/backend/controllers/EnrollmentController.java
@@ -1,27 +1,32 @@
 package com.wburda.skillforest.backend.controllers;
 
 import com.wburda.skillforest.backend.dto.StudentCourseEnrollmentDTO;
-import com.wburda.skillforest.backend.services.CourseService;
+import com.wburda.skillforest.backend.services.CourseEnrollmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/enrollments")
 public class EnrollmentController {
-    private final CourseService courseService;
+    private final CourseEnrollmentService courseEnrollmentService;
 
     @Autowired
-    public EnrollmentController(CourseService courseService) {
-        this.courseService = courseService;
+    public EnrollmentController(CourseEnrollmentService courseEnrollmentService) {
+        this.courseEnrollmentService = courseEnrollmentService;
     }
 
     @GetMapping
     public ResponseEntity<List<StudentCourseEnrollmentDTO>> getCourseEnrollmentsForCurrentStudent() {
-        return ResponseEntity.ok(courseService.getAllStudentCourseEnrollmentForCurrentStudent());
+        return ResponseEntity.ok(courseEnrollmentService.getAllStudentCourseEnrollmentForCurrentStudent());
+    }
+
+    @PostMapping("/{courseId}")
+    public ResponseEntity<Void> enrollStudentInCourse(@PathVariable UUID courseId) {
+        courseEnrollmentService.enrollStudentInCourse(courseId);
+        return ResponseEntity.noContent().build();
     }
 }
--- backend/src/main/java/com/wburda/skillforest/backend/services/CourseService.java
+++ backend/src/main/java/com/wburda/skillforest/backend/services/CourseService.java
@@ -2,13 +2,9 @@ package com.wburda.skillforest.backend.services;
 
 import com.wburda.skillforest.backend.dto.CourseDTO;
 import com.wburda.skillforest.backend.dto.CourseRequestDTO;
-import com.wburda.skillforest.backend.dto.StudentCourseEnrollmentDTO;
 import com.wburda.skillforest.backend.entities.Course;
-import com.wburda.skillforest.backend.entities.CourseEnrollment;
-import com.wburda.skillforest.backend.entities.Student;
 import com.wburda.skillforest.backend.entities.Teacher;
 import com.wburda.skillforest.backend.mappers.CourseMapper;
-import com.wburda.skillforest.backend.repositories.CourseEnrollmentRepository;
 import com.wburda.skillforest.backend.repositories.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,7 +16,6 @@ import java.util.UUID;
 @Service
 public class CourseService {
     private final CourseMapper courseMapper;
-    private final CourseEnrollmentRepository courseEnrollmentRepository;
     private final CourseRepository courseRepository;
     private final UserService userService;
 
@@ -28,17 +23,10 @@ public class CourseService {
     private String frontendUrl;
 
     @Autowired
-    public CourseService(CourseMapper courseMapper, CourseEnrollmentRepository courseEnrollmentRepository, UserService userService, CourseRepository courseRepository) {
+    public CourseService(CourseMapper courseMapper, UserService userService, CourseRepository courseRepository) {
         this.courseMapper = courseMapper;
-        this.courseEnrollmentRepository = courseEnrollmentRepository;
         this.courseRepository = courseRepository;
         this.userService = userService;
-    }
-
-    public List<StudentCourseEnrollmentDTO> getAllStudentCourseEnrollmentForCurrentStudent() {
-        Student currentStudent = userService.getCurrentlyLoggedStudent();
-        List<CourseEnrollment> courseEnrollments = courseEnrollmentRepository.findByStudent(currentStudent);
-        return courseEnrollments.stream().map(courseMapper::toStudentCourseEnrollmentDTO).toList();
     }
 
     public List<CourseDTO> getAllCoursesForCurrentTeacher() {
